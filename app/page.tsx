5cf4fd88b0b5703cb7eec6ec0bd6b948dc0b9e34
grantace2001 <156390469+grantace2001@users.noreply.github.com>
2024-10-08 15:42:58 -0400

    changing layout

--- app/page.tsx
+++ app/page.tsx
@@ -7,48 +7,30 @@ import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
+import "./../app/app.css";
 
 Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
 
 export default function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }
-
-  useEffect(() => {
-    listTodos();
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
-    });
-  }
-
   return (
     <main>
-      <h1 className="right-heading">Grace Assembly of God</h1>
-      <h2 className="right-heading">Spring City, PA</h2>
-      <h2>My things to do</h2>
-      <button onClick={createTodo}>+ new</button>
+    {/* Navigation Bar */}
+    <nav className="navbar">
       <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#contact">Contact</a></li>
       </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
-    </main>
-  );
+    </nav>
+
+    {/* Header */}
+    <h1 className="right-heading">Grace Assembly of God</h1>
+    <h2 className="right-heading">Spring City, PA</h2>
+
+    {/* Add any other content for the landing page here */}
+  </main>
+);
 }
