a0ca4984350c114aa35369b12690e66a63dfb9d2
grantace2001 <156390469+grantace2001@users.noreply.github.com>
2024-10-09 14:42:10 -0400

    supa

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { Amplify } from "aws-amplify";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
-import "./../app/app.css";
-import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 import "./../app/app.css";
@@ -16,7 +14,11 @@ const client = generateClient<Schema>();
 export default function App() {
   return (
     <main style={{ paddingTop: '60px' }}>
-      <h1>Grace Assembly of God</h1>
+      {/* Header for logo or title */}
+      <header className="logo-banner">
+        <h1>Grace Assembly of God</h1>
+      </header>
+
       {/* Navigation Bar */}
       <nav className="navbar">
         <a href="#Home">Home</a>
@@ -29,14 +31,11 @@ export default function App() {
         <a href="#Resources">Resources</a>
         <a href="#Give">Give</a>
       </nav>
-      {/* Header */}
-      {/*will put a video or something here*/}
 
-      {/* Add any other content for the landing page here */}
       {/* Centered Text */}
-  <div className="centered-text">
-    <p>JOIN US FOR CHURCH THIS SUNDAY!</p>
-  </div>
+      <div className="centered-text">
+        <p>JOIN US FOR CHURCH THIS SUNDAY!</p>
+      </div>
     </main>
   );
 }
