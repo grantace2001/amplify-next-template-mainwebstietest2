6e355d29cb9728e3b44d30e98fcc0bd60684fa8d
grantace2001 <156390469+grantace2001@users.noreply.github.com>
2024-10-09 15:10:52 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function App() {
     <main>
       {/* Header for logo or title */}
       <header className="logo-banner">
-        <h1>Website Title or Logo</h1> {/* You can replace this with an actual logo image */}
+        <h1>Grace Assembly of God</h1> {/* You can replace this with an actual logo image */}
       </header>
 
       {/* Navigation Bar */}
