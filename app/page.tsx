b16d30146034c0bfd5802307fb1c22ec811b15d9
grantace2001 <156390469+grantace2001@users.noreply.github.com>
2024-10-10 12:11:46 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 import "./../app/app.css";
 import Link from "next/link";
-import ContactForm from './components/ContactForm'; // Correct path
 
 
 Amplify.configure(outputs);
